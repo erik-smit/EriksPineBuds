7e3f05e8a276ab52bd8fba537b8f28271afd82f7
erik-smit <erik.lucas.smit@gmail.com>
2025-11-04 13:26:07 +0100

    Release v1.2.0

--- services/ble_profiles/opb_config/opb_config_common.h
+++ services/ble_profiles/opb_config/opb_config_common.h
@@ -69,7 +69,7 @@ typedef struct {
 }
 
 #define OPB_CONFIG_VERSION_MAJOR 1
-#define OPB_CONFIG_VERSION_MINOR 1
+#define OPB_CONFIG_VERSION_MINOR 2
 #define OPB_CONFIG_VERSION_PATCH 0
 
 #ifdef __cplusplus
